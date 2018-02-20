7a7aa2275b181a483789f8506b91892531b8143a
ard333 <ard333.ardiansyah@gmail.com>
2018-02-20 14:38:39 +0700

    project init

---
+++ src/main/java/id/web/ard/springbootwebfluxmongodbreactive/SpringBootWebfluxMongodbReactiveApplication.java
@@ -0,0 +1,12 @@
+package id.web.ard.springbootwebfluxmongodbreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxMongodbReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxMongodbReactiveApplication.class, args);
+	}
+}
